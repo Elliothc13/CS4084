a73cd771a8c1bd5328d6eb89e82e83bb9c1a4d45
dawidK5 <19233116@studentmail.ul.ie>
2022-04-09 00:59:30 +0100

    Add basic maps functionality

--- app/src/main/java/com/example/myapplication/HomeActivity.java
+++ app/src/main/java/com/example/myapplication/HomeActivity.java
@@ -8,22 +8,34 @@ import android.view.View;
 import android.widget.Button;
 
 public class HomeActivity extends AppCompatActivity {
-    private Button button;
+    private Button settingsButton, mapButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.home_screen);
 
-        button = findViewById(R.id.settingsButton);
-        button.setOnClickListener(new View.OnClickListener() {
+        settingsButton = findViewById(R.id.settingsButton);
+        settingsButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                openActivityMap();
+                openActivitySettings();
             }
         });
+        mapButton = findViewById(R.id.mapButton);
+        mapButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) { openActivityMap(); }
+        });
+
     }
-    public void openActivityMap(){
+
+    public void openActivityMap() {
+        Intent intent = new Intent(this, MapActivity.class);
+        startActivity(intent);
+    }
+
+    public void openActivitySettings(){
         Intent intent = new Intent(this, SettingsActivity.class);
         startActivity(intent);
     }
--- app/src/main/java/com/example/myapplication/MapActivity.java
+++ app/src/main/java/com/example/myapplication/MapActivity.java
@@ -5,22 +5,36 @@ import androidx.fragment.app.Fragment;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Parcelable;
 import android.view.View;
 import android.widget.Button;
 
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 
-public class MapActivity extends AppCompatActivity {
-    private Button button;
+public class MapActivity extends AppCompatActivity implements OnMapReadyCallback {
 
+    private GoogleMap map;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.settings);
+        setContentView(R.layout.map_activity);
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
 
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        map = googleMap;
+        LatLng Limerick = new LatLng(52.663157, -8.619842);
+        map.addMarker(new MarkerOptions().position(Limerick).title("Limerick"));
+        map.moveCamera(CameraUpdateFactory.newLatLng(Limerick));
     }
-    public void openActivityHome(){
-        Intent intent = new Intent(this, HomeActivity.class);
-        startActivity(intent);
-    }
+
 }
