e77a711bfe315aa52456608a80f2c103443f5501
dawidK5 <19233116@studentmail.ul.ie>
2022-04-14 00:34:16 +0100

    Add Firestore handling and callback

--- app/src/main/java/com/example/myapplication/AuthActivity.java
+++ app/src/main/java/com/example/myapplication/AuthActivity.java
@@ -2,21 +2,40 @@ package com.example.myapplication;
 
 import androidx.activity.result.ActivityResultCallback;
 import androidx.activity.result.ActivityResultLauncher;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.SystemClock;
+import android.util.Log;
+import android.widget.Toast;
 
 import com.firebase.ui.auth.AuthUI;
 import com.firebase.ui.auth.FirebaseAuthUIActivityResultContract;
 import com.firebase.ui.auth.IdpResponse;
 import com.firebase.ui.auth.data.model.FirebaseAuthUIAuthenticationResult;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.SetOptions;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.EventListener;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.atomic.AtomicBoolean;
 
 public class AuthActivity extends AppCompatActivity {
+    public static final String CLASS_NAME = AuthActivity.class.getSimpleName();
     private final ActivityResultLauncher<Intent> signInLauncher = registerForActivityResult(
             new FirebaseAuthUIActivityResultContract(),
             new ActivityResultCallback<FirebaseAuthUIAuthenticationResult>() {
@@ -29,11 +48,31 @@ public class AuthActivity extends AppCompatActivity {
 
     private void onSignInResult(FirebaseAuthUIAuthenticationResult result) {
         IdpResponse response = result.getIdpResponse();
-        if (result.getResultCode() == RESULT_OK && FirebaseAuth.getInstance().getCurrentUser() != null) {
-            System.out.println("===== Sign-in successful =====");
-            openHomeActivity();
+        FirebaseUser currentUser = FirebaseAuth.getInstance().getCurrentUser();
+        if (result.getResultCode() == RESULT_OK && currentUser != null) {
+            System.out.println("===== Sign-in successful");
+            if (response.isNewUser()) {
+                Activity currentActivity = this;
+                createProfile(currentUser, new CallbackForAsync() {
+                    @Override
+                    public void onCallback(boolean s) {
+                        if (s) {
+                            openHomeActivity();
+                        } else {
+                            Log.e(CLASS_NAME, "===== Sign in failed, Firestore error");
+                            Toast.makeText(getApplicationContext(),
+                                    "Firestore error: please login again", Toast.LENGTH_LONG).show();
+                            SystemClock.sleep(1000);
+                            currentActivity.onBackPressed();
+                        }
+                    }
+                });
+            } else {
+                openHomeActivity();
+            }
+
         } else {
-            System.out.println("===== Sign in failed: error: " + response.getError() + " =====");
+            Log.e(CLASS_NAME, "===== Sign in failed: error: " + response.getError());
         }
     }
 
@@ -53,5 +92,26 @@ public class AuthActivity extends AppCompatActivity {
     public void openHomeActivity(){
         Intent intent = new Intent(this, HomeActivity.class);
         startActivity(intent);
+    }
+
+    public void createProfile(FirebaseUser user, CallbackForAsync cb) {
+        String idToken = user.getUid();
+        FirebaseFirestore database = FirebaseFirestore.getInstance();
+        final List<String> defaultEmpty = new ArrayList<>();
+        Map<String, Object> profileDefaults = new HashMap<>();
+        profileDefaults.put("name", user.getDisplayName());
+        profileDefaults.put("businessOwner", false);
+        profileDefaults.put("posts", defaultEmpty);
+        profileDefaults.put("routines", defaultEmpty);
+        database.collection("users").document(idToken)
+                .set(profileDefaults, SetOptions.merge())
+                .addOnCompleteListener(new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        boolean comp = task.isComplete();
+                        boolean success = task.isSuccessful();
+                        cb.onCallback(comp && success);
+                    }
+                });
     }
 }
---
+++ app/src/main/java/com/example/myapplication/CallbackForAsync.java
@@ -0,0 +1,5 @@
+package com.example.myapplication;
+
+public interface CallbackForAsync {
+    void onCallback (boolean s);
+}
