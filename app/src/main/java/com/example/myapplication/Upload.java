04afbd81ee09052f071fb1a8817bf959388cef41
ElliotCleary <19264259@studentmail.ul.ie>
2022-04-13 22:34:49 +0100

    can now upload images to firestore

---
+++ app/src/main/java/com/example/myapplication/Upload.java
@@ -0,0 +1,32 @@
+package com.example.myapplication;
+
+public class Upload {
+    private String mName;
+    private String mImageUrl;
+
+    public Upload() {
+        //empty constructor needed.
+    }
+
+    public Upload(String name, String imageUrl) {
+        if (mName.trim().equals("")) {
+            name = "-";
+        }
+
+        mName = name;
+        mImageUrl = imageUrl;
+    }
+
+    public String getName() {
+        return mName;
+    }
+    public void setName(String name){
+        mName = name;
+    }
+    public String getImageUrl() {
+        return mImageUrl;
+    }
+    public void setImageUrl(String imageUrl){
+        mImageUrl = imageUrl;
+    }
+}
--- app/src/main/java/com/example/myapplication/UploadActivity.java
+++ app/src/main/java/com/example/myapplication/UploadActivity.java
@@ -1,16 +1,176 @@
 package com.example.myapplication;
 
+import android.content.ContentResolver;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Handler;
 import android.view.View;
+import android.webkit.MimeTypeMap;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.ProgressBar;
 import android.widget.TextView;
-import com.kosalgeek.android.photoutil.GalleryPhoto;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.OnProgressListener;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+
 
 public class UploadActivity extends AppCompatActivity {
-    //private final String TAG = this.getClass().getName();
+
+    private final static int PICK_IMAGE_REQUEST = 1;
+
+    private Button buttonChooseImage, buttonUpload;
+    private TextView textViewShowUploads;
+    private EditText editTextFileName;
+    private ImageView imageView;
+    private ProgressBar progressBar;
+
+    private Uri imageUri;
+
+    private StorageReference storageRef = FirebaseStorage.getInstance().getReference("uploads");
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_upload2);
+
+        buttonChooseImage = findViewById(R.id.button_choose_image);
+        buttonUpload = findViewById(R.id.button_upload);
+        textViewShowUploads = findViewById(R.id.text_view_show_uploads);
+        editTextFileName = findViewById(R.id.edit_test_file_name);
+        imageView = findViewById(R.id.image_view);
+        progressBar = findViewById(R.id.progress_bar);
+
+        buttonChooseImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openFileChooser();
+            }
+        });
+        buttonUpload.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                uploadFile();
+            }
+        });
+        textViewShowUploads.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+    }
+    private void openFileChooser() {
+        Intent intent = new Intent();
+        intent.setType("image/*");
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+        startActivityForResult(intent, PICK_IMAGE_REQUEST);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == PICK_IMAGE_REQUEST && resultCode == RESULT_OK && data != null && data.getData() != null){
+            imageUri = data.getData();
+            // Picasso.with(this).load(imageUri).into(imageView);
+            imageView.setImageURI(imageUri);
+        }
+    }
+
+    private String getFileExtension(Uri uri){
+        ContentResolver cR = null;
+        cR = getContentResolver();
+        MimeTypeMap mime = MimeTypeMap.getSingleton();
+        return mime.getExtensionFromMimeType(cR.getType(uri));
+    }
+
+    private void uploadFile(){
+        if (imageUri != null){
+            StorageReference fileReference = storageRef.child(System.currentTimeMillis() + "." + getFileExtension(imageUri));
+
+            fileReference.putFile(imageUri)
+                    .addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                        @Override
+                        public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                            Handler handler = new Handler();
+                            handler.postDelayed(new Runnable() {
+                                @Override
+                                public void run() {
+                                    //progressBar.setProgress(0);
+                                }
+                            }, 5000);
+
+                            Toast.makeText(UploadActivity.this, "Upload Successful", Toast.LENGTH_SHORT).show();
+                            //Upload upload = new Upload(editTextFileName.getText().toString().trim(), taskSnapshot.getDownloadUrl().toString());
+                        }
+                    })
+                    .addOnFailureListener(new OnFailureListener() {
+                        @Override
+                        public void onFailure(@NonNull Exception e) {
+                            Toast.makeText(UploadActivity.this, e.getMessage(), Toast.LENGTH_SHORT).show();
+                        }
+                    })
+                    .addOnProgressListener(new OnProgressListener<UploadTask.TaskSnapshot>() {
+                        @Override
+                        public void onProgress(@NonNull UploadTask.TaskSnapshot snapshot) {
+                            progressBar.setProgress(100);
+                        }
+                    });
+        }else{
+            Toast.makeText(this, "no image", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    /*private final String TAG = this.getClass().getName();
 
     ImageView imageView, galleryView;
     TextView textView;
@@ -49,6 +209,6 @@ public class UploadActivity extends AppCompatActivity {
                 //Bitmap bitmap = ImageLoader.init().from(photoPath).requestSize(128, 128).getBitmap();
             }
         }
-    }
+    }*/
 }
 
