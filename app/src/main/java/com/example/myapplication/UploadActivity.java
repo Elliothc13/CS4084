60bd5523a842537384bb6fd66bce5289e6b17e26
ElliotCleary <19264259@studentmail.ul.ie>
2022-04-13 19:30:02 +0100

    fixed upload page

--- app/src/main/java/com/example/myapplication/UploadActivity.java
+++ app/src/main/java/com/example/myapplication/UploadActivity.java
@@ -1,24 +1,19 @@
 package com.example.myapplication;
 
 import android.content.Intent;
-import android.graphics.Bitmap;
 import android.net.Uri;
 import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
-
-import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
-import android.widget.Toast;
+import android.widget.TextView;
 import com.kosalgeek.android.photoutil.GalleryPhoto;
-import com.kosalgeek.android.photoutil.ImageLoader;
-
-import java.io.FileNotFoundException;
 
 public class UploadActivity extends AppCompatActivity {
     //private final String TAG = this.getClass().getName();
 
     ImageView imageView, galleryView;
+    TextView textView;
 
     GalleryPhoto galleryPhoto;
 
@@ -33,6 +28,7 @@ public class UploadActivity extends AppCompatActivity {
 
         imageView = (ImageView)findViewById(R.id.imageView);
         galleryView = (ImageView)findViewById(R.id.galleryView);
+        textView = (TextView)findViewById(R.id.textView);
 
         galleryView.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -46,21 +42,11 @@ public class UploadActivity extends AppCompatActivity {
         super.onActivityResult(requestCode, resultCode, data);
         if (resultCode == RESULT_OK) {
             if (requestCode == GALLERY_REQUEST) {
-                Log.i("hi", "hi");
                 Uri uri = data.getData();
                 galleryPhoto.setPhotoUri(uri);
                 String photoPath = galleryPhoto.getPath();
-                try {
-                    Log.i("hi", "just before bitmap");
-                    Bitmap bitmap = ImageLoader.init().from(photoPath).requestSize(128, 128).getBitmap();
-                    if (bitmap != null) {
-                        imageView.setImageBitmap(bitmap);
-                    }else{
-                        Log.i("hi", "bitmap didnt work");
-                    }
-                } catch (FileNotFoundException e) {
-                    Toast.makeText(getApplicationContext(), "Something went wrong while loading the image", Toast.LENGTH_SHORT).show();
-                }
+                textView.setText(uri.toString());
+                //Bitmap bitmap = ImageLoader.init().from(photoPath).requestSize(128, 128).getBitmap();
             }
         }
     }
