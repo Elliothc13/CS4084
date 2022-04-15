9fd09ed3a1bffa3eca857523075faf313e648bde
ElliotCleary <19264259@studentmail.ul.ie>
2022-04-15 22:38:27 +0100

    added people tab

--- app/src/main/java/com/example/myapplication/MapActivity.java
+++ app/src/main/java/com/example/myapplication/MapActivity.java
@@ -34,6 +34,21 @@ public class MapActivity extends AppCompatActivity implements OnMapReadyCallback
         map = googleMap;
         LatLng Limerick = new LatLng(52.663157, -8.619842);
         map.addMarker(new MarkerOptions().position(Limerick).title("Limerick"));
+
+        LatLng GoGymLimerick = new LatLng(52.67461838920839, -8.67740927457319);
+        map.addMarker(new MarkerOptions().position(GoGymLimerick).title("GO GYM Limerick"));
+
+        LatLng CrossFitTreaty = new LatLng(52.674530819622305, -8.688913909418236);
+        map.addMarker(new MarkerOptions().position(CrossFitTreaty).title("CrossFit Treaty"));
+
+        LatLng TUSSportshub = new LatLng(52.67586268807097, -8.649654378521578);
+        map.addMarker(new MarkerOptions().position(TUSSportshub).title("TUS Sportshub"));
+
+        LatLng EnergizeHealthClub = new LatLng(52.665932960086586, -8.63165026187101);
+        map.addMarker(new MarkerOptions().position(EnergizeHealthClub).title("Energize Health Club"));
+
+        LatLng APGPersonalTraining = new LatLng(52.665932960086586, -8.63165026187101);
+        map.addMarker(new MarkerOptions().position(APGPersonalTraining).title("APG Personal Training"));
         map.moveCamera(CameraUpdateFactory.newLatLng(Limerick));
     }
 
--- app/src/main/java/com/example/myapplication/SettingsFragment.java
+++ app/src/main/java/com/example/myapplication/SettingsFragment.java
@@ -3,18 +3,40 @@ package com.example.myapplication;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import java.util.ArrayList;
+
 
 public class SettingsFragment extends Fragment {
+    private RecyclerView mRecyclerView;
+    private RecyclerView.Adapter mAdapter;
+    private RecyclerView.LayoutManager mLayoutManager;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_settings, container, false);
+        ArrayList<Item> exampleList = new ArrayList<>();
+        exampleList.add(new Item(R.drawable.ic_baseline_account_circle_24, "Line1", "Line2"));
+        exampleList.add(new Item(R.drawable.ic_baseline_account_circle_24, "Line3", "Line4"));
+        exampleList.add(new Item(R.drawable.ic_baseline_account_circle_24, "Line5", "Line6"));
+
+        mRecyclerView = (RecyclerView) rootView.findViewById(R.id.recyclerView);
+        mRecyclerView.setHasFixedSize(true);
+        mLayoutManager = new LinearLayoutManager(this.getContext());
+        mAdapter = new Adapter(exampleList);
+
+        mRecyclerView.setLayoutManager(mLayoutManager);
+        mRecyclerView.setAdapter(mAdapter);
+
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_settings, container, false);
+        return rootView;
     }
+
 }
